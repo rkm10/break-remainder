8f2b7f854680ce00bbec1a9e05ef4c89c19498e3
rkm10 <rajkumarmalluri08@gmail.com>
2024-12-13 14:36:55 +0530

    base url updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/break-remainder/' // Ensure your server serves this subdirectory
-})
+  base: '/break-remainder/' // Ensure the trailing slash
+});
