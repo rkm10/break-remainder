c81e37b2a5e113d559739a0bd1b6e03aee810dea
rkm10 <rajkumarmalluri08@gmail.com>
2024-12-13 14:28:44 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/break-remainder/' // Ensure your server serves this subdirectory
+  // base: '/break-remainder/' // Ensure your server serves this subdirectory
 })
