7657aa0ee2c5c04fdf25292fab73b34eeb7428ae
rkm10 <rajkumarmalluri08@gmail.com>
2024-12-13 14:27:33 +0530

    checking for errors

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'break-remainder'
+  base: '/break-remainder/' // Ensure your server serves this subdirectory
 })
